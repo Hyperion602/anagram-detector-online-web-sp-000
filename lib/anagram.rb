f3e7c2013c5deac0336abb5acf0b922d55fa8788
James Harris <mharris9608@yahoo.com>
2021-02-17 17:53:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,14 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
+  
   def intialize(word)
     @word = word
   end
 
   def match(anagram)
-
+    anagram.each do |words|
+      words.split("").sort == @word.split("").sort)
+    end      
+  end
 end
